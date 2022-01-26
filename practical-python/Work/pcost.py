76dcebb4d38767d1f594a3a03798a328c3669c52
JayThibs <thibo.jacques@gmail.com>
2022-01-25 20:49:23 -0400

    Created program to calculate total cost of stock portfolio.

--- practical-python/Work/pcost.py
+++ practical-python/Work/pcost.py
@@ -1,3 +1,14 @@
-# pcost.py
-#
-# Exercise 1.27
+from pathlib import Path
+
+filepath = str(Path(".").resolve() / "practical-python/Work/Data/portfolio.csv")
+total_cost = 0
+
+with open(filepath, "rt") as f:
+    headers = next(f)
+    for line in f:
+        row = line.split(",")
+        share = int(row[1])
+        price = float(row[2])
+        total_cost += share * price
+
+print(f"Total cost: {total_cost:0.2f}")
