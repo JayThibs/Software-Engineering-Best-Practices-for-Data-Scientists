e8eda4ac3b0208267524a6b5e062d2661d61da20
JayThibs <thibo.jacques@gmail.com>
2022-01-26 21:27:30 -0400

    Refactored code into a function.

--- practical-python/Work/pcost.py
+++ practical-python/Work/pcost.py
@@ -1,14 +1,28 @@
 from pathlib import Path
 
 filepath = str(Path(".").resolve() / "practical-python/Work/Data/portfolio.csv")
-total_cost = 0
 
-with open(filepath, "rt") as f:
-    headers = next(f)
-    for line in f:
-        row = line.split(",")
-        share = int(row[1])
-        price = float(row[2])
-        total_cost += share * price
 
-print(f"Total cost: {total_cost:0.2f}")
+def portfolio_cost(filepath: str) -> int:
+    """
+    Calculates the total cost of a stock portfolio.
+
+    Args:
+        filepath (str): The filepath to the CSV file containing the stock portfolio.
+    Returns:
+        total_cost (float): Returns the total cost of the stock portfolio.
+    """
+    total_cost = 0
+    with open(filepath, "rt") as f:
+        headers = next(f)
+        for line in f:
+            row = line.split(",")
+            share = int(row[1])
+            price = float(row[2])
+            total_cost += share * price
+
+        return total_cost
+
+
+cost = portfolio_cost(filepath)
+print(f"Total cost: {cost:0.2f}")
