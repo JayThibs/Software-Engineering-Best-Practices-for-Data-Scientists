c2c9502f6aa999a930ead1d56dd9a709dd59388d
JayThibs <thibo.jacques@gmail.com>
2022-01-28 20:55:14 -0400

    Created script to read portfolio to create a report.

--- practical-python/Work/report.py
+++ practical-python/Work/report.py
@@ -1,3 +1,22 @@
-# report.py
-#
-# Exercise 2.4
+import csv
+import sys
+from pathlib import Path
+
+
+def read_portfolio(filename: str):
+    portfolio = []
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    return print(portfolio)
+
+
+if len(sys.argv) == 2:
+    filepath = sys.argv[1]
+else:
+    filepath = str(Path(".").resolve() / "practical-python/Work/Data/portfolio.csv")
+
+read_portfolio(filepath)
