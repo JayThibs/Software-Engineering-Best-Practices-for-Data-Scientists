099334d7b481a5f85411fd9711d51a59c611d8bf
JayThibs <thibo.jacques@gmail.com>
2022-05-24 23:08:04 -0300

    Added cost for portfolio.

--- practical-python/Work/report.py
+++ practical-python/Work/report.py
@@ -5,13 +5,28 @@ from pathlib import Path
 
 def read_portfolio(filename: str):
     portfolio = []
-    with open(filename, "rt") as f:
+    with open(filename) as f:
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
             holding = (row[0], int(row[1]), float(row[2]))
             portfolio.append(holding)
     return print(portfolio)
+
+
+def portfolio_cost(filename: str) -> float:
+    """Computes the total cost (shares*price) of a portfolio file."""
+    total_cost = 0.0
+
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            nshares = int(row[1])
+            price = float(row[2])
+            total_cost += nshares * price
+    return total_cost
+        
 
 
 if len(sys.argv) == 2:
